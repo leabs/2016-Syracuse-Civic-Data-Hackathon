a58134baa7c9e5cc11e81fbb09ba0468e8caeb28
leabs <stevenleabo@gmail.com>
2016-10-09 15:38:44 -0400

    Final push for contest
    Lots and lots of changes, added a 4th graph for dashboard, added
    searching list function, style changes, etc

---
+++ roadData.js
@@ -0,0 +1,88 @@
+$(function(){
+
+var roadData = [
+
+
+{"year": 1981, "totals":	9, "rating":	4.555555556, "crack":	4.609756098},
+{"year": 1982, "totals":	2, "rating":	4.5, "crack":	3},
+{"year": 1983, "totals":	22, "rating":	4.590909091, "crack":	2.272727273},
+{"year": 1984, "totals":	6, "rating":	4, "crack":	3.333333333},
+{"year": 1985, "totals":	19, "rating":	4.842105263, "crack":	2.473684211},
+{"year": 1986, "totals":	32, "rating":	5.4375, "crack":	2.90625},
+{"year": 1987, "totals":	55, "rating":	5.109090909, "crack":	2.909090909},
+{"year": 1988, "totals":	68, "rating":	5.161764706, "crack":	2.911764706},
+{"year": 1989, "totals":	118, "rating":	5.483050847, "crack":	2.915254237},
+{"year": 1990, "totals":	94, "rating":	5.393617021, "crack":	2.808510638},
+{"year": 1991, "totals":	115, "rating":	5.426086957, "crack":	2.982608696},
+{"year": 1992, "totals":	100, "rating":	5.61, "crack":	2.84},
+{"year": 1993, "totals":	94, "rating":	5.031914894, "crack":	2.85106383},
+{"year": 1994, "totals":	86, "rating":	5.662790698, "crack":	2.953488372},
+{"year": 1995, "totals":	67, "rating":	5.880597015, "crack":	3.014925373},
+{"year": 1996, "totals":	116, "rating":	5.603448276, "crack":	3.00862069},
+{"year": 1997, "totals":	92, "rating":	6.065217391, "crack":	3.065217391},
+{"year": 1998, "totals":	177, "rating":	5.581920904, "crack":	2.966101695},
+{"year": 1999, "totals":	165, "rating":	5.606060606, "crack":	2.993939394},
+{"year": 2000, "totals":	153, "rating":	5.843137255, "crack":	2.934640523},
+{"year": 2001, "totals":	265, "rating":	5.78490566, "crack":	2.947169811},
+{"year": 2002, "totals":	347, "rating":	5.714697406, "crack":	2.976945245},
+{"year": 2003, "totals":	186, "rating":	6.451612903, "crack":	3.38172043},
+{"year": 2004, "totals":	439, "rating":	6.23690205, "crack":	3.166287016},
+{"year": 2005, "totals":	253, "rating":	7, "crack":	3.553359684},
+{"year": 2006, "totals":	77, "rating":	7.376623377, "crack":	3.857142857},
+{"year": 2007, "totals":	37, "rating":	7.72972973, "crack":	3.918918919},
+{"year": 2008, "totals":	61, "rating":	6.819672131, "crack":	3.213114754},
+{"year": 2009, "totals":	51, "rating":	7.725490196, "crack":	4.058823529},
+{"year": 2010, "totals":	24, "rating":	8.041666667, "crack":	4.083333333},
+{"year": 2011, "totals":	88, "rating":	8.227272727, "crack":	4.363636364},
+{"year": 2012, "totals":	35, "rating":	8.314285714, "crack":	4.485714286},
+{"year": 2013, "totals":	78, "rating":	9.628205128, "crack":	4.923076923},
+{"year": 2014, "totals":	35, "rating":	9.828571429, "crack":	5},
+{"year": 2015, "totals":	25, "rating":	10, "crack":	5}
+
+    ];
+
+
+
+
+
+var chart = new tauCharts.Chart({
+            guide: {
+              x: {
+                    label: {text: 'Year of Last Overlay'},
+                    "tickFormat": "Y",
+                    nice:false
+                  },
+
+                  y: {
+                        label: {text: 'Road Rating'},
+
+                        nice:false
+                      },
+
+                    },
+
+            data: roadData,
+            type: 'scatterplot',
+            x: 'year',
+            y: 'rating',
+            color: 'crack',
+            size: 'totals',
+            plugins: [
+            	tauCharts.api.plugins.get('tooltip')({fields:[ "year", "rating", "crack", "totals"]}),
+              tauCharts.api.plugins.get('legend')(),
+              tauCharts.api.plugins.get('quick-filter')()
+
+
+
+            ]
+
+
+
+
+        })
+
+
+chart.renderTo('#scatterplot');
+
+
+});
